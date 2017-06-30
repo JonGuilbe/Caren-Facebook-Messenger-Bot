725cefaf21334bc710a61ebada5d9af073958064
Jonathan Guilbe <jguilbe@knights.ucf.edu>
2017-06-30 12:03:21 -0400

    Attempt #3

--- webhook.js
+++ webhook.js
@@ -136,7 +136,7 @@ function sendMessage(event) {
   //console.log(text);
     apiai.on('response', (response) => {
   let aiText = response.result.fulfillment.speech;
-  console.log("Sauce is " + response.result);
+  console.log("Sauce is " + response.result.fufillment);
   if(response.source == 'reddit-img'){
     request({
       url: 'https://graph.facebook.com/v2.6/me/messages',
